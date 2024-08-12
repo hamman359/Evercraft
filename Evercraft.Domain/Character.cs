80df587df641368b89c155febee67dc5bcfb94e7
Jonathan Hammond <hamman359@gmail.com>
2024-08-11 22:11:54 -0400

    Can get character name.

---
+++ Evercraft.Domain/Character.cs
@@ -0,0 +1,11 @@
+﻿namespace Evercraft.Domain;
+
+public class Character
+{
+    public Character(string characterName)
+    {
+        Name = characterName;
+    }
+
+    public string Name { get; private set; }
+}
--- Evercraft/GlobalUsings.cs
+++ Evercraft/GlobalUsings.cs
@@ -1,5 +1,2 @@
-﻿global using Evercraft.Domain.Entities;
-global using Evercraft.Domain.Enums;
-global using Evercraft.Domain.ValueObjects;
-
+﻿
 global using FluentAssertions;
